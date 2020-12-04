58a3b916bfe116b39d71d00b062cca657346f30b
mohsen <mohsen.abedelaal@gmail.com>
2020-12-04 17:34:43 +0000

    14

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -4,14 +4,9 @@ module
 """
 
 
-import numpy as np
-
-
-def np_cat(mat1, mat2, axis=0):
+def np_elementwise(mat1, mat2):
     """
     function
     """
 
-    if not axis:
-        return np.vstack((mat1, mat2))
-    return np.hstack((mat1, mat2))
+    return mat1 + mat2, mat1 - mat2, mat1*mat2, mat1/mat2
