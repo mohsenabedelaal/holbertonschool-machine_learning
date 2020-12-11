9cb2b70f86a8a8ed61d413759944d0e496510f44
mohsen <mohsen.abedelaal@gmail.com>
2020-12-11 21:36:06 +0000

    11

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-"""Sum"""
+"""summation i squared"""
 
 
 def summation_i_squared(n):
     """Module for sum"""
     if type(n) != int or n < 1:
         return None
-    else:
-        return (n * (n + 1) * (2 * n + 1)) // 6
+    return (n * (n + 1) * (2 * n + 1)) // 6
