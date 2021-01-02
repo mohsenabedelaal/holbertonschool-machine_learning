48ed99f96413979e26d2ed8bcad6937e0aa09215
mohsen <mohsen.abedelaal@gmail.com>
2021-01-02 08:58:47 +0000

    c-3

---
+++ supervised_learning/0x01-classification/.#0-neuron.py
@@ -0,0 +1,1 @@
+vagrant@vagrant-ubuntu-trusty-64.1958:1609576509
---
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -0,0 +1,30 @@
+#ort numpy as np
+
+
+class Neuron:
+    """ Class that models a Neuron"""
+    def __init__(self, nx):
+        """ Class constructor for Neuron"""
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.__W = np.ndarray((1, nx))
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """ Getter for weights"""
+        return self.__W
+
+    @property
+    def b(self):
+        """ Getter for bias"""
+        return self.__b
+
+    @property
+    def A(self):
+        """ Getter for activated output"""
+        return self.__A
