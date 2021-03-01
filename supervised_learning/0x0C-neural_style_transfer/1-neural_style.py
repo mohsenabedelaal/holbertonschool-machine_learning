46a8266f0c459e685e6f9f07dc415a26dd207678
mohsen <mohsen.abedelaal@gmail.com>
2021-03-01 12:44:20 +0200

    fix 01

--- supervised_learning/0x0C-neural_style_transfer/1-neural_style.py
+++ supervised_learning/0x0C-neural_style_transfer/1-neural_style.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 """
-Create a class NST that performs tasks for neural style transfer
+Neural style transfer class
 """
-import numpy as np
+
 import tensorflow as tf
-tf.enable_eager_execution()
+import numpy as np
 
 
 class NST:
     """
-    Class NST
+    A class including model designed to do style transfer between two images
     """
     style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
                     'block4_conv1', 'block5_conv1']
@@ -17,65 +17,54 @@ class NST:
 
     def __init__(self, style_image, content_image, alpha=1e4, beta=1):
         """
-        Class constructor
+        Initialize the class.
+        @style_image: image for style to mimic
+        @content_image: image we're modifying to match style
+        @alpha: learning rate for content
+        @beta: learning rate for style
         """
-        if not isinstance(style_image, np.ndarray) or \
-           len(style_image.shape) != 3 or \
-           style_image.shape[2] != 3:
-            raise TypeError(
-                "style_image must be a numpy.ndarray with shape (h, w, 3)")
-        if not isinstance(content_image, np.ndarray) or \
-           len(content_image.shape) != 3 or \
-           content_image.shape[2] != 3:
-            raise TypeError(
-                "content_image must be a numpy.ndarray with shape (h, w, 3)")
-        if not isinstance(alpha, (int, float)) or alpha < 0:
+        tf.enable_eager_execution()
+        if ((not isinstance(style_image, np.ndarray)
+             or len(style_image.shape) != 3 or style_image.shape[2] != 3)):
+            raise TypeError("style_image must be a numpy.ndarray "
+                            "with shape (h, w, 3)")
+        if ((not isinstance(content_image, np.ndarray)
+             or len(content_image.shape) != 3 or content_image.shape[2] != 3)):
+            raise TypeError("content_image must be a "
+                            "numpy.ndarray with shape (h, w, 3)")
+        if ((not isinstance(alpha, (float, int, complex))
+             or alpha < 0)):
             raise TypeError("alpha must be a non-negative number")
-        if not isinstance(beta, (int, float)) or beta < 0:
+        if ((not isinstance(beta, (float, int, complex))
+             or beta < 0)):
             raise TypeError("beta must be a non-negative number")
         self.style_image = self.scale_image(style_image)
         self.content_image = self.scale_image(content_image)
         self.alpha = alpha
         self.beta = beta
         self.load_model()
-        self.model = model
 
     @staticmethod
     def scale_image(image):
         """
-        Rescales an image such that its values are between 0 and 1
-        and its largest side is 512 pixels
-        Returns the scaled image
+        Scales an image to be between 0-1 and resizes max dim to 512
         """
-        if not isinstance(image, np.ndarray) or \
-           len(image.shape) != 3 or \
-           image.shape[2] != 3:
-            raise TypeError(
-                "image must be a numpy.ndarray with shape (h, w, 3)")
-        height = image.shape[0]
-        width = image.shape[1]
-        scale = 512 / max(height, width)
-        new_shape = (int(scale * height), int(scale * width))
-        image = np.expand_dims(image, axis=0)
-        image_scaled = tf.clip_by_value(tf.image.resize_bicubic
-                                        (image, new_shape) / 255.0, 0.0, 1.0)
-        return image_scaled
+        max_side = max(image.shape)
+        dims = [int(image.shape[0] / max_side * 512),
+                int(image.shape[1] / max_side * 512)]
+        image = tf.compat.v1.image.resize_bicubic(image[np.newaxis, ...], dims)
+        image -= tf.reduce_min(image)
+        return image / tf.reduce_max(image)
 
     def load_model(self):
         """
-        Creates the model used to calculate cost
+        Loads VGG19 keras model
         """
-        vgg = tf.keras.applications.vgg19.VGG19(include_top=False)
-        x = vgg.input
-
-        for layer in vgg.layers[1:]:
-            layer.trainable = False
-            if isinstance(layer, tf.keras.layers.MaxPooling2D):
-                x = tf.keras.layers.AveragePooling2D(name=layer.name)(x)
-            else:
-                x = layer(x)
-        global model
-        model = tf.keras.models.Model(vgg.input, x, name="model")
-        outputs = [model.get_layer(layer).get_output_at(1)
-                   for layer in self.style_layers + [self.content_layer]]
-        model = tf.keras.models.Model(vgg.input, outputs)
+        base_model = tf.keras.applications.vgg19.VGG19(include_top=False)
+        base_model.trainable = False
+        layers = self.style_layers + [self.content_layer]
+        model = tf.keras.Model(inputs=base_model.input,
+                               outputs=[base_model.get_layer(layer).output
+                                        for layer in layers])
+        self.model = model
+        return self.model
