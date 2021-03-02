0228c45d0222ee4f00e53b3a4ac38eb59ab8be92
mohsen <mohsen.abedelaal@gmail.com>
2021-03-02 10:34:58 +0200

    7

--- supervised_learning/0x03-optimization/7-RMSProp.py
+++ supervised_learning/0x03-optimization/7-RMSProp.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-"""creates training operation
-   using gradient descent with RMSProp
 """
-import tensorflow as tf
+Updates a variable using the RMSProp optimization algorithm
+"""
+import numpy as np
 
-
-def create_RMSProp_op(loss, alpha, beta2, epsilon):
-    """returns tensor with RMSProp"""
-    optimizer = tf.train.RMSPropOptimizer(learning_rate=alpha,
-                                          decay=beta2, epsilon=epsilon)
-    train = optimizer.minimize(loss)
-    return train
+def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    """
+    Returns: the updated variable and the new moment, respectively
+    """
+    S =  beta2 * s + (1 - beta2) * np.power(grad, 2)
+    W = var - alpha * grad/(np.sqrt(S)+epsilon)
+    return W, S
